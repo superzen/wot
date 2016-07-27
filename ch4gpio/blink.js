5ce84dfe823a97d7953058c9e2afbb4a9e3a23a4
Jesus Ciriano <super.zen@gmail.com>
2016-07-27 20:04:15 +0200

    Initial commit

---
+++ ch4gpio/blink.js
@@ -0,0 +1,20 @@
+var onfoff = require('onoff');
+
+var Gpio = onoff.Gpio,
+    led = new Gpio(4, 'out'),
+    interval;
+
+interval = setInterval(function () {
+    var nextValue = (led.readSync() + 1) % 2;
+    led.write(nextValue, function () {
+        console.log('Changed LED state to: ' + nextValue);
+    })
+}, 2000);
+
+process.on('SIGINT', function () {
+    clearInterval(interval);
+    led.writeSync(0);
+    led.unexport();
+    console.log('Bye bye!');
+    process.exit();
+});
